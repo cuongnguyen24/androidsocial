714ab90779a57d6428973d231f8df9da5140539b
dabi61 <daodinhbinh612004@gmail.com>
2024-10-12 14:23:53 +0700

    Initial commit

---
+++ app/src/main/java/com/utt/tt21/cc_modulelogin/MainActivity.java
@@ -0,0 +1,125 @@
+package com.utt.tt21.cc_modulelogin;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.utt.tt21.cc_modulelogin.adapter.ViewPager2Adapter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TableLayout tableLayout;
+    private ViewPager2 viewPager2;
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Thiết lập các insets cho cửa sổ
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+
+        // Khởi tạo nút đăng xuất
+        //btnLogout = findViewById(R.id.btn_logout);
+
+        // Sự kiện OnClickListener cho nút đăng xuất
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut(); // Đăng xuất khỏi Firebase
+//                Intent intent = new Intent(MainActivity.this, SignInActivity.class); // Chuyển về màn hình đăng ký
+//                startActivity(intent);
+//                finishAffinity(); // Đóng tất cả các activity trước đó
+//            }
+//        });
+
+        initId();
+        ViewPager2Adapter adapter = new ViewPager2Adapter(this);
+        viewPager2.setAdapter(adapter);
+        ChangePage();
+        viewPager2.setOffscreenPageLimit(1);
+
+    }
+
+    private void ChangePage() {
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position)
+                {
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.action_search).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.action_add).setChecked(true);
+                        break;
+                    case 3:
+                        bottomNavigationView.getMenu().findItem(R.id.action_notification).setChecked(true);
+                        break;
+                    case 4:
+                        bottomNavigationView.getMenu().findItem(R.id.action_profile).setChecked(true);
+                        break;
+
+                }
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.action_home)
+                {
+                    viewPager2.setCurrentItem(0);
+                } else if (item.getItemId() == R.id.action_search) {
+                    viewPager2.setCurrentItem(1);
+                }
+                else if (item.getItemId() == R.id.action_add) {
+                    viewPager2.setCurrentItem(2);
+                }
+                else if (item.getItemId() == R.id.action_notification)
+                {
+                    viewPager2.setCurrentItem(3);
+                }
+                else if (item.getItemId() == R.id.action_profile)
+                {
+                    viewPager2.setCurrentItem(4);
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+    private void initId() {
+        viewPager2 = findViewById(R.id.viewPager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+    }
+}
---
+++ app/src/main/java/com/utt/tt21/cc_modulelogin/authentication/SignUpActivity.java
@@ -0,0 +1,103 @@
+package com.utt.tt21.cc_modulelogin.authentication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.utt.tt21.cc_modulelogin.MainActivity;
+import com.utt.tt21.cc_modulelogin.R;
+
+public class SignUpActivity extends AppCompatActivity {
+    private LinearLayout layoutSignIn;
+    private EditText edtEmail, edtPassword, edtConfirmPassword;
+    private Button btnSignUp;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        initUi();
+        initListener();
+    }
+
+    private void initListener() {
+        layoutSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSignUp();
+            }
+        });
+    }
+
+    private void onClickSignUp() {
+        String strEmail = edtEmail.getText().toString().trim();
+        String strPassword = edtPassword.getText().toString().trim();
+        String strConfirmPassword = edtConfirmPassword.getText().toString().trim();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (strEmail.isEmpty() || strPassword.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Kiểm tra xem mật khẩu và xác nhận mật khẩu có khớp nhau không
+        if (!strPassword.equals(strConfirmPassword)) {
+            Toast.makeText(this, "Mật khẩu không khớp, vui lòng thử lại", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.show();
+
+        auth.createUserWithEmailAndPassword(strEmail, strPassword)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finishAffinity(); // đóng tất cá các activity trước MainActivity
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void initUi(){
+        layoutSignIn = findViewById(R.id.layout_sign_in);
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+        edtConfirmPassword = findViewById(R.id.edt_confirm_password);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Đang đăng ký...");
+    }
+}
