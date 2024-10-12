04235ef608b2ec20f25a89828951587cbfc0a8f8
cuongnguyen24 <cuongngba7@gmail.com>
2024-10-12 16:11:24 +0700

    update log out

--- app/src/main/java/com/utt/tt21/cc_modulelogin/fragment/Profile.java
+++ app/src/main/java/com/utt/tt21/cc_modulelogin/fragment/Profile.java
@@ -1,5 +1,6 @@
 package com.utt.tt21.cc_modulelogin.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.utt.tt21.cc_modulelogin.R;
+import com.utt.tt21.cc_modulelogin.authentication.SignInActivity;
 
 
 public class Profile extends Fragment {
@@ -21,6 +25,22 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Khởi tạo nút đăng xuất
+        Button btnLogout = view.findViewById(R.id.btn_logout);
+
+        // Sự kiện OnClickListener cho nút đăng xuất
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut(); // Đăng xuất khỏi Firebase
+                Intent intent = new Intent(getActivity(), SignInActivity.class); // Chuyển về màn hình đăng nhập
+                startActivity(intent);
+                getActivity().finishAffinity(); // Đóng tất cả các activity trước đó
+            }
+        });
+
+        return view;
     }
 }
